64f4bbc89ab86d37327f001914d2c69acb0111d9
Jmackey <jmackey@jmackey.com>
2023-04-29 15:33:33 -0600

    Restart/Exit And boundaries

--- scripts/GameManager/GameManager.cs
+++ scripts/GameManager/GameManager.cs
@@ -4,8 +4,8 @@ using System;
 public partial class GameManager : Node
 {
 	[Export] public int BabyCount { get; set; }
-	public GameState CurrentState { get; set; }
-
+	public GameState CurrentState { get => currentState; set { currentState = value; GD.Print("Game State changed to: ", value); } }
+	public Baby LaunchedBaby { get; set; }
 
 	public static GameManager Instance;
 
@@ -21,10 +21,14 @@ public partial class GameManager : Node
 	[Export] private Label ScoreValueLabel;
 	[Export] private Label BabyCountLabel;
 	[Export] private PanelContainer GameOverPanel;
+	[Export] private Button RestartButton;
+	[Export] private Button ExitButton;
 
 	private int playerScore;
 	private int babyCount;
+	private int initialBabyCount;
 	private GameState currentState;
+	private Baby launchedBaby;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -40,6 +44,10 @@ public partial class GameManager : Node
 		{
 			throw new Exception("BabyCount not defined in inspector");
 		}
+		RestartButton.ButtonDown += RestartGame;
+		ExitButton.ButtonDown += ExitGame;
+
+		initialBabyCount = BabyCount;
 		// TODO: Figure out state flow
 		CurrentState = GameState.Play;
 		playerScore = 0;
@@ -52,17 +60,29 @@ public partial class GameManager : Node
 		switch (CurrentState)
 		{
 			case GameState.Play:
+				if (IsInstanceValid(LaunchedBaby))
+				{
+					CurrentState = GameState.Waiting;
+					return;
+				}
 				if (BabyCount <= 0)
 				{
 					GD.Print("ENDING GAME");
 					CurrentState = GameState.End;
 					GameOverPanel.Visible = true;
 				}
-				break;
+			break;
+
+			case GameState.Waiting:
+				if(!IsInstanceValid(LaunchedBaby))
+				{
+					CurrentState = GameState.Play;
+					return;
+				}
+			break;
 		}
 		
 	}
-
 	private void UpdateUI()
 	{
 		ScoreValueLabel.Text = playerScore.ToString();
@@ -72,4 +92,17 @@ public partial class GameManager : Node
 	{
 		playerScore += amount;
 	}
+
+	private void RestartGame()
+	{
+		playerScore = 0;
+		BabyCount = initialBabyCount;
+		GameOverPanel.Visible = false;
+		CurrentState = GameState.Play;
+	}
+	private void ExitGame()
+	{
+		GetTree().Quit();
+	}
+	
 }
