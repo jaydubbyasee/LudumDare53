09ff5e47d1dabfd45e3f04117a263a5f408a299a
Jmackey <jmackey@jmackey.com>
2023-04-29 14:47:19 -0600

    Finite Baby Count and Game Over

--- scripts/GameManager/GameManager.cs
+++ scripts/GameManager/GameManager.cs
@@ -3,37 +3,73 @@ using System;
 
 public partial class GameManager : Node
 {
-	public int PlayerScore { get => playerScore; set => playerScore = value; }
-	public int BabyCount { get => babyCount; set => babyCount = value; }
+	[Export] public int BabyCount { get; set; }
+	public GameState CurrentState { get; set; }
 
-	public GameManager Instance;
+
+	public static GameManager Instance;
+
+	public enum GameState
+	{
+		MainMenu,
+		Ready,
+		Play,
+		Waiting,
+		End,
+	}
 
 	[Export] private Label ScoreValueLabel;
+	[Export] private Label BabyCountLabel;
+	[Export] private PanelContainer GameOverPanel;
 
 	private int playerScore;
 	private int babyCount;
-
-	// Probably general game States here
-	// Ready countdown
-	// Playing
-	// Game ended
+	private GameState currentState;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GameOverPanel.Visible = false;
+
+		if(Instance != null)
+		{
+			throw new Exception("Duplicate game manager detected!");
+		}
 		Instance = this;
+		if (BabyCount <= 0)
+		{
+			throw new Exception("BabyCount not defined in inspector");
+		}
+		// TODO: Figure out state flow
+		CurrentState = GameState.Play;
 		playerScore = 0;
-		babyCount = 5;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		UpdateUI();
+		switch (CurrentState)
+		{
+			case GameState.Play:
+				if (BabyCount <= 0)
+				{
+					GD.Print("ENDING GAME");
+					CurrentState = GameState.End;
+					GameOverPanel.Visible = true;
+				}
+				break;
+		}
+		
 	}
 
 	private void UpdateUI()
 	{
-		ScoreValueLabel.Text = PlayerScore.ToString();
+		ScoreValueLabel.Text = playerScore.ToString();
+		BabyCountLabel.Text = BabyCount.ToString();
+	}
+	public void UpdateScore(int amount)
+	{
+		playerScore += amount;
 	}
 }
--- scripts/catcher/CatcherController.cs
+++ scripts/catcher/CatcherController.cs
@@ -4,17 +4,17 @@ using System;
 public partial class CatcherController : Node2D
 {
 	[Export] private float moveSpeed;
-	private GameManager gameManager;
+	//private GameManager gameManager;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		//GD.Print(GetTree().CurrentScene.FindChild("GameManager", true));
-		gameManager = GetTree().CurrentScene.FindChild("GameManager", true) as GameManager;
-		if (gameManager == null)
-		{
-			throw new Exception("Cannot find GameManager at /");
-		}
+		////GD.Print(GetTree().CurrentScene.FindChild("GameManager", true));
+		//gameManager = GetTree().CurrentScene.FindChild("GameManager", true) as GameManager;
+		//if (gameManager == null)
+		//{
+		//	throw new Exception("Cannot find GameManager at /");
+		//}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -43,7 +43,7 @@ public partial class CatcherController : Node2D
 			GD.Print("Max Height was: ", baby.MaxHeight);
 			GD.Print("Max Distance was: ", baby.MaxDistance);
 			int score = CalcScore(baby.MaxHeight, baby.MaxDistance);
-			gameManager.PlayerScore += score;
+			GameManager.Instance.UpdateScore(score);
 			baby.Destroy();
 		}
 	}
